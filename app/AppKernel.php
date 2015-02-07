e31ee26009821c83b010687df68eab980fd96347
Jerzy Lekowski <jerzy@lekowski.pl>
2015-02-07 20:12:02 +0000

    Config for JMSSerializerBundle and FOSRestBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new RestApi\Bundle\RestApiBundle(),
+            new \JMS\SerializerBundle\JMSSerializerBundle(),
+            new \FOS\RestBundle\FOSRestBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
